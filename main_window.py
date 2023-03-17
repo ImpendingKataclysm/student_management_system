de7d696b3749e64d025053f205c066812a412df4
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-03-17 10:36:19 -0700

    Added a toolbar with icons for searching and adding students

--- main_window.py
+++ main_window.py
@@ -1,5 +1,5 @@
-from PyQt6.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem
-from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem, QToolBar
+from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtCore import Qt
 from insert_dialog import InsertDialog
 from search_dialog import SearchDialog
@@ -19,27 +19,34 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 600)
 
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
         edit_menu_item = self.menuBar().addMenu("&Edit")
 
-        add_student_action = QAction("Add Student", self)
+        add_student_action = QAction(QIcon("icons/add.png"), "Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
 
-        edit_action = QAction("Search", self)
-        edit_action.triggered.connect(self.search)
-        edit_menu_item.addAction(edit_action)
+        search_action = QAction(QIcon("icons/search.png"), "Search", self)
+        search_action.triggered.connect(self.search)
+        edit_menu_item.addAction(search_action)
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Phone"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+        toolbar.addAction(add_student_action)
+        toolbar.addAction(search_action)
 
     def load_data(self):
         """
